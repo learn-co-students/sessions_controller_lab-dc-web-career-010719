ea8876a41a6b4c5280cedd464966c49994c3b067
hai-nguyen1112 <hainguyen871112@gmail.com>
2019-02-09 14:05:19 -0500

    done

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    session[:name] = params[:name]
+    if !session[:name].nil? && !session[:name].empty?
+      redirect_to '/'
+    else
+      redirect_to '/login'
+    end
+  end
+
+  def destroy
+    if !session[:name].nil?
+      session.delete :name
+      redirect_to '/login'
+    else
+      redirect_to '/login'
+    end
+  end
+
+end
